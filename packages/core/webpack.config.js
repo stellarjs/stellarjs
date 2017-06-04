91270b8fbfa46e04bd9dbf3b3a2d9cd063e64405
Andres Olave <andres.olave@thinkingbytes.net>
2017-06-04 09:23:37 +0300

    fix for webpack builds

--- packages/core/webpack.config.js
+++ packages/core/webpack.config.js
@@ -4,4 +4,6 @@ webpackConfig.entry = {
   'index': `${__dirname}/src/index.browser.js`
 };
 
+console.info(`Library: ${webpackConfig.output.library}`);
+
 module.exports = webpackConfig;
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
+const last = require('lodash/last');
 
 function webpackConfig(context) {
   return {
@@ -10,9 +11,12 @@ function webpackConfig(context) {
     entry: {
       index: `${context}/src/index.js`,
     },
+    target: 'node',
     output: {
       path: path.resolve(context, 'lib'),
       filename: '[name].browser.js',
+      library: `@stellarjs/${last(context.split('/'))}`,
+      libraryTarget: 'umd',
     },
     externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
     module: {
