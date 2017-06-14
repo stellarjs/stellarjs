7d4f44c162df2a731a19beab3e227873246c6247
Andres Olave <andres.olave@thinkingbytes.net>
2017-06-14 20:50:30 +0300

    add error to message

--- packages/transport-socket/src/WebsocketTransport.js
+++ packages/transport-socket/src/WebsocketTransport.js
@@ -80,7 +80,7 @@ class WebsocketTransport {
       try {
         callback(command);
       } catch (e) {
-        this.log.warn('invalid message sent to stellar websocket transport');
+        this.log.warn('invalid message sent to stellar websocket transport', e);
       }
     });
     return Promise.resolve();
