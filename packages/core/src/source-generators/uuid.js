0cd16ed0a456d9b38cb595c1110dcb8ced49fc10
Andres Olave <andres.olave@thinkingbytes.net>
2017-06-14 19:21:29 +0300

    simplify uuid import

--- packages/core/src/source-generators/uuid.js
+++ packages/core/src/source-generators/uuid.js
@@ -1,10 +1,10 @@
 /**
  * Created by arolave on 29/05/2017.
  */
-import uuid from 'uuid';
+import uuid from 'uuid/v4';
 import Promise from 'bluebird';
 import { getFromInstanceId } from './common';
 
 export default function run() {
-  return Promise.resolve(getFromInstanceId(uuid.v4()));
+  return Promise.resolve(getFromInstanceId(uuid()));
 }
