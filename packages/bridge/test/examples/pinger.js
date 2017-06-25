f2e294ca7f7eda07d379ad80f94a2b43e04f0d82
Andres Olave <andres.olave@thinkingbytes.net>
2017-06-25 17:36:48 +0300

    fix bridge

--- packages/bridge/test/examples/pinger.js
+++ packages/bridge/test/examples/pinger.js
@@ -5,26 +5,23 @@ import { stellarAppPubSub, stellarHandler, StellarError } from '@stellarjs/core'
 import url from 'url';
 
 import boot from '../../src/bootstrap';
-import bridgeInit from '../../src/server';
 
-const server = boot();
-bridgeInit({
-             server,
-             newSessionHandlers: [
-               ({ log, socket, session }) => {
-                const request = socket.request;
-                const parsedUrl = url.parse(request.url, true);
-                const userId = parsedUrl.query['x-auth-user'];
+boot({
+  newSessionHandlers: [
+   ({ log, socket, session }) => {
+    const request = socket.request;
+    const parsedUrl = url.parse(request.url, true);
+    const userId = parsedUrl.query['x-auth-user'];
 
-                if (userId === '3') {
-                  throw new StellarError('Authentication Error');
-                } else if (userId === '4') {
-                  throw new Error('Other Error');
-                }
+    if (userId === '3') {
+      throw new StellarError('Authentication Error');
+    } else if (userId === '4') {
+      throw new Error('Other Error');
+    }
 
-                return session;
-              },
-             ],
+    return session;
+  },
+  ],
 });
 
 const PUBLISH_ACTIONS = {
