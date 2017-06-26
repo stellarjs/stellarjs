1d0043cdb4f5122675a6166104d27e6ebf9fd905
Andres Olave <andres.olave@thinkingbytes.net>
2017-06-26 17:19:43 +0300

    make setBluebirdScheduler available

--- packages/core/src/index.browser.js
+++ packages/core/src/index.browser.js
@@ -1,11 +1,18 @@
 /**
  * Created by arolave on 10/04/2017.
  */
+import Promise from 'bluebird';
+
 import { setSourceGenerators } from './factory';
 import browser from './source-generators/browser';
 
+function setBluebirdScheduler(fn) {
+  Promise.setScheduler(fn);
+}
+
 setSourceGenerators('browser', { browser });
 
+export { setBluebirdScheduler };
 export { logger } from './logger';
 export { StellarError } from './StellarError';
 export { default as StellarCore } from './StellarCore';
