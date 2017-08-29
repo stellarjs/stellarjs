f35b1a4e67b6f17ba2c82f79649262c1521d932c
Moshe Demri <mdemri@gmail.com>
2017-08-29 21:32:40 +0300

    fix bug + tests

--- packages/mw-newrelic/src/index.js
+++ packages/mw-newrelic/src/index.js
@@ -21,6 +21,6 @@ export default function (req, next) {
                   newrelic.endTransaction();
                   reject(err);
                 });
-    })();
+    });
   });
 }
--- packages/mw-newrelic/test/mw-newrelic.test.js
+++ packages/mw-newrelic/test/mw-newrelic.test.js
@@ -1,8 +1,33 @@
 /**
  * Created by arolave on 14/05/2017.
  */
-xdescribe('no tests', () => {
-  xit('no test', () => {
-    // fail();
+
+import Promise from 'bluebird';
+import set from 'lodash/set';
+import middleware from '../src';
+
+describe('Test NR middleware use cases', () => {
+
+  it('Should pass request w/o queue to the next midddleware ', () => {
+    const req = {};
+    const val = 'success';
+    const next = () => Promise.resolve(val);
+
+    middleware(req, next)
+      .then(res => expect(res).toBe(val));
+  });
+
+  it('Should pass request with queue to NR and to the next midddleware ', () => {
+    const req = {};
+    set(req, 'headers.queueName', 'queueName')
+    const val = 'success';
+    const resolve = () => Promise.resolve(val);
+
+    middleware(req, resolve)
+      .then(res => expect(res).toBe(val));
+    
+    const reject = () => Promise.reject(val);
+    middleware(req, reject)
+      .catch(res => expect(res).(val));
   });
 });
