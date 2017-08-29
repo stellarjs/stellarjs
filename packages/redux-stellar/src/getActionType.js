b39a83b21d0e6f741ee42d0f534b6c3134695c31
Andres Olave <andres@thinkingbytes.net>
2017-08-29 10:30:04 +0300

    lint fixes

--- packages/redux-stellar/src/getActionType.js
+++ packages/redux-stellar/src/getActionType.js
@@ -1,7 +1,6 @@
 const FULFILLED = 'FULFILLED';
 const PENDING = 'PENDING';
 const REJECTED = 'REJECTED';
-const UPDATED = 'UPDATED';
 const UNSUBSCRIBE = 'UNSUBSCRIBE';
 
 const DOCUMENT_ADDED = 'DOCUMENT_ADDED';
@@ -9,38 +8,38 @@ const DOCUMENT_UPDATED = 'DOCUMENT_UPDATED';
 const DOCUMENT_REMOVED = 'DOCUMENT_REMOVED';
 
 export default function getActionType(action) {
-    const theAction = typeof action === 'function' ? action() : action;
-    if (theAction.type) {
-        return theAction.type;
-    }
-    const pathSuffix = theAction.path ? `:${theAction.path}` : '';
-    return `${theAction.resource}:${theAction.method}${pathSuffix}`;
+  const theAction = typeof action === 'function' ? action() : action;
+  if (theAction.type) {
+    return theAction.type;
+  }
+  const pathSuffix = theAction.path ? `:${theAction.path}` : '';
+  return `${theAction.resource}:${theAction.method}${pathSuffix}`;
 }
 
 export function getPendingActionType(action) {
-    return `${getActionType(action)}_${PENDING}`;
+  return `${getActionType(action)}_${PENDING}`;
 }
 
 export function getFulfilledActionType(action) {
-    return `${getActionType(action)}_${FULFILLED}`;
+  return `${getActionType(action)}_${FULFILLED}`;
 }
 
 export function getRejectedActionType(action) {
-    return `${getActionType(action)}_${REJECTED}`;
+  return `${getActionType(action)}_${REJECTED}`;
 }
 
 export function getUnsubscribeActionType(action) {
-    return `${getActionType(action)}_${UNSUBSCRIBE}`;
+  return `${getActionType(action)}_${UNSUBSCRIBE}`;
 }
 
 export function getDocAddedActionType(action) {
-    return `${getActionType(action)}_${DOCUMENT_ADDED}`;
+  return `${getActionType(action)}_${DOCUMENT_ADDED}`;
 }
 
 export function getDocRemovedActionType(action) {
-    return `${getActionType(action)}_${DOCUMENT_REMOVED}`;
+  return `${getActionType(action)}_${DOCUMENT_REMOVED}`;
 }
 
 export function getDocUpdatedActionType(action) {
-    return `${getActionType(action)}_${DOCUMENT_UPDATED}`;
+  return `${getActionType(action)}_${DOCUMENT_UPDATED}`;
 }
