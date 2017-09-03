f38bf31379fc8886ce20a69ae3a08c74db966119
Moshe Demri <mdemri@gmail.com>
2017-09-03 14:39:32 +0300

    done()

--- packages/mw-newrelic/test/mw-newrelic.test.js
+++ packages/mw-newrelic/test/mw-newrelic.test.js
@@ -7,31 +7,34 @@ import set from 'lodash/set';
 import middleware from '../src';
 
 describe('New Relic middleware', () => {
-  it('Should pass request w/o queue to the next midddleware ', () => {
+  it('Should pass request w/o queue to the next midddleware ', (done) => {
     const req = {};
     const val = 'success';
     const next = () => Promise.resolve(val);
 
     middleware(req, next)
-      .then(res => expect(res).toEqual(val));
+      .then(res => expect(res).toEqual(val))
+      .finally(() => done());
   });
 
-  it('Should pass request with queue to NR and to the next midddleware (success)', () => {
+  it('Should pass request with queue to NR and to the next midddleware (success)', (done) => {
     const req = {};
     set(req, 'headers.queueName', 'queueName');
     const val = 'success';
     const resolve = () => Promise.resolve(val);
 
     middleware(req, resolve)
-      .then(res => expect(res).toEqual(val));
+      .then(res => expect(res).toEqual(val))
+      .finally(() => done());
   });
 
-  it('Should pass request with queue to NR and to the next midddleware (failure)', () => {
+  it('Should pass request with queue to NR and to the next midddleware (failure)', (done) => {
     const req = {};
     set(req, 'headers.queueName', 'queueName');
     const val = 'reject';
     const reject = () => Promise.reject(val);
     middleware(req, reject)
-      .catch(res => expect(res).toEqual(val));
+      .catch(res => expect(res).toEqual(val))
+      .finally(() => done());
   });
 });
