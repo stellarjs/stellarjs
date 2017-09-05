98b02a297a6c60e5b487fd8903ce8a4dee5ce31c
Moshe Demri <mdemri@gmail.com>
2017-09-05 11:09:15 +0300

    Upgrade to Bull 3.1

--- packages/transport-redis/src/Enqueuer.js
+++ packages/transport-redis/src/Enqueuer.js
@@ -2,6 +2,8 @@
  * Created by arolave on 02/02/2017.
  */
 import Job from 'bull/lib/job';
+import Queue from 'bull/lib/queue';
+import RedisClient from './config-redisclient';
 
 class Enqueuer {
   constructor(name, { keyPrefix, client }) {
@@ -15,11 +17,12 @@ class Enqueuer {
   }
 
   add(data, opts) {
-    return Job.create(this, data, opts);
+    
+    return Job.create(new Queue(this.name, this.client), data, opts);
   }
 
   distEmit() { // eslint-disable-line class-methods-use-this
-        // do nothing
+    // do nothing
   }
 }
 
