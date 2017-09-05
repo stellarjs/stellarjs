20d39ac5a56caa5dca1b1bdd9471af6d42259d62
Moshe Demri <mdemri@gmail.com>
2017-09-05 11:15:10 +0300

    lint

--- packages/transport-redis/src/Enqueuer.js
+++ packages/transport-redis/src/Enqueuer.js
@@ -3,8 +3,6 @@
  */
 import Job from 'bull/lib/job';
 import Queue from 'bull/lib/queue';
-import RedisClient from './config-redisclient';
-
 class Enqueuer {
   constructor(name, { keyPrefix, client }) {
     this.client = client;
@@ -17,7 +15,6 @@ class Enqueuer {
   }
 
   add(data, opts) {
-    
     return Job.create(new Queue(this.name, this.client), data, opts);
   }
 
