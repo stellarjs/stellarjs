01c0fe47d3263deca9b9ac328be96ad2017ca4a2
Gefen <shailevi@gmail.com>
2017-11-08 23:30:22 +0200

    add instrument

--- packages/instrument-newrelic/src/index.js
+++ packages/instrument-newrelic/src/index.js
@@ -1,40 +1,39 @@
-import _ from 'lodash';
+import get from 'lodash/get';
 
-// eslint-disable-next-line better-mutation/no-mutation
-module.exports = function instrumentRedisTransport(shim, messageBrokerModule) {
-    shim.setLibrary('stellarjs');
+export default function instrumentRedisTransport(shim, messageBrokerModule) {
+  shim.setLibrary('stellarjs');
 
-    shim.recordProduce(messageBrokerModule.RedisTransport.prototype, 'enqueue', (_shim, fn, name, args) => {
-        const queueName = args[0];
-        const obj = (args.length > 1) ? args[1] : {};
+  shim.recordProduce(messageBrokerModule.RedisTransport.prototype, 'enqueue', (_shim, fn, name, args) => {
+    const queueName = args[0];
+    const obj = (args.length > 1) ? args[1] : {};
 
         // The message headers must be pulled to enable cross-application tracing.
         // const headers = options ? options.headers : {};
-        const headers = _.get(obj, 'headers');
-        const body = _.get(obj, 'body');
+    const headers = get(obj, 'headers');
+    const body = get(obj, 'body');
 
         // misc key/value parameters can be recorded as a part of the trace segment
-        return {
-            callback: _shim.LAST,
-            destinationName: _.get(headers, 'queueName', queueName),
-            destinationType: _shim.QUEUE,
-            headers,
-            parameters: body,
-        };
-    });
+    return {
+      callback: _shim.LAST,
+      destinationName: get(headers, 'queueName', queueName),
+      destinationType: _shim.QUEUE,
+      headers,
+      parameters: body,
+    };
+  });
 
-    shim.recordSubscribedConsume(messageBrokerModule.RedisTransport.prototype, 'process', {
-        consumer: shim.LAST,
-        messageHandler(_shim, consumer, name, args) {
-            const job = args[0];
-            const headers = _.get(job, 'data.headers', {});
-            const queueName = _.get(headers, 'queueName', job.queue.name);
+  shim.recordSubscribedConsume(messageBrokerModule.RedisTransport.prototype, 'process', {
+    consumer: shim.LAST,
+    messageHandler(_shim, consumer, name, args) {
+      const job = args[0];
+      const headers = get(job, 'data.headers', {});
+      const queueName = get(headers, 'queueName', job.queue.name);
 
-            return {
-                destinationName: queueName,
-                destinationType: _shim.QUEUE,
-                headers,
-            };
-        },
-    });
-};
+      return {
+        destinationName: queueName,
+        destinationType: _shim.QUEUE,
+        headers,
+      };
+    },
+  });
+}
