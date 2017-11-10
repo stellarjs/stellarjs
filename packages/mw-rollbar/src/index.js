ecd6d0e0dd2798dde924b455a20bac6a934bee54
arolave <andres@thinkingbytes.net>
2017-11-10 19:17:22 +0200

    lint fixes

--- packages/mw-rollbar/src/index.js
+++ packages/mw-rollbar/src/index.js
@@ -4,35 +4,34 @@ import isError from 'lodash/isError';
 import map from 'lodash/map';
 import Promise from 'bluebird';
 import rollbar from 'rollbar';
-import { StellarError } from '@stellarjs/core';
 
 export function rollbarMiddlewareConfigurer({ ignoredErrorTypes } = {}) {
     /* reimplementation of rollbar.errorHandler() for promise based middleware */
-    return function (req, next) {
-        return new Promise((resolve, reject) => {
-            next()
+  return function (req, next) {
+    return new Promise((resolve, reject) => {
+      next()
               .then(response => resolve(response))
               .catch((err) => {
-                  function cb(rollbarErr) {
-                      if (rollbarErr) {
-                          this.log.warn(`Error reporting to rollbar, ignoring: ${rollbarErr}`);
-                      }
-
-                      reject(err);
+                function cb(rollbarErr) {
+                  if (rollbarErr) {
+                    this.log.warn(`Error reporting to rollbar, ignoring: ${rollbarErr}`);
                   }
 
-                  if (includes(map(ignoredErrorTypes, 'name'), get(err, 'constructor.name'))) {
-                      return reject(err);
-                  }
+                  reject(err);
+                }
 
-                  if (err instanceof Error || isError(err)) {
-                      return rollbar.handleError(err, req, cb);
-                  }
+                if (includes(map(ignoredErrorTypes, 'name'), get(err, 'constructor.name'))) {
+                  return reject(err);
+                }
 
-                  return rollbar.reportMessage(`Error: ${err}`, 'error', req, cb);
+                if (err instanceof Error || isError(err)) {
+                  return rollbar.handleError(err, req, cb);
+                }
+
+                return rollbar.reportMessage(`Error: ${err}`, 'error', req, cb);
               });
-        });
-    }
+    });
+  };
 }
 
 export default rollbarMiddlewareConfigurer();
