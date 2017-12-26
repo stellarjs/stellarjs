2691c93d1a65278825903fb10b1e3a161dd9ca63
arolave <andres@thinkingbytes.net>
2017-12-26 15:06:54 +0200

    cleanup hander

--- packages/core/src/StellarHandler.js
+++ packages/core/src/StellarHandler.js
@@ -9,11 +9,10 @@ import isFunction from 'lodash/isFunction';
 import isPlainObject from 'lodash/isPlainObject';
 import lowerCase from 'lodash/lowerCase';
 import map from 'lodash/map';
-import omit from 'lodash/omit';
+import omitBy from 'lodash/omitBy';
 
 import Promise from 'bluebird';
 
-import { StellarError } from './StellarError';
 import StellarCore from './StellarCore';
 
 export default class StellarHandler extends StellarCore {
@@ -31,39 +30,17 @@ export default class StellarHandler extends StellarCore {
 
     StellarHandler.isProcessing.add(serviceInbox);
     this.log.info(`@StellarHandler: Starting processing`, { inbox: serviceInbox });
-    this._process(serviceInbox, (job) => {
-      const jobData = job.data;
-
-      const logComplete = (result, e) => {
-        if (!e) {
-          return result;
-        } else if (e instanceof StellarError) {
-          // eslint-disable-next-line max-len
-          this.log.warn(`@StellarHandler error`, { requestId: jobData.headers.id, errorMessageKeys: e.messageKeys() });
-        } else {
-          this.log.error(omit(e, '__stellarResponse'));
-          // eslint-disable-next-line max-len
-          this.log.error(`@StellarHandler error`, { requestId: jobData.headers.id, errorMessage: e.message });
-        }
-
-        return result;
-      };
-
-      const sendResponse = (response, e) =>
-            this._enqueue(jobData.headers.respondTo, response)
-              .then(() => logComplete(response, e));
-
+    this._process(serviceInbox, ({ data }) => {
       return this
-            ._executeMiddlewares(this.allMiddlewares, jobData)
-            .then(response => sendResponse(response))
-            .catch((e) => {
-              if (e.__stellarResponse != null) {
-                return sendResponse(e.__stellarResponse, e);
-              }
-
-              this.log.error(e, `@StellarHandler Unexpected error`, { requestId: jobData.headers.id });
-              throw e;
-            });
+        ._executeMiddlewares(this.allMiddlewares, data)
+        .then(response => this._enqueue(data.headers.respondTo, response))
+        .catch((e) => {
+          this.log.error(omitBy(e, (val, key) => key === '__stellarResponse' || isFunction(val)), '@StellarHandler ERROR');
+          if (e.__stellarResponse != null) {
+            return this._enqueue(data.headers.respondTo, e.__stellarResponse);
+          }
+          throw e;
+        });
     });
   }
 
