0f169e513f4e12dd22ceba7ad9cf6fd687d64a1c
arolave <andres@thinkingbytes.net>
2018-02-14 16:04:06 +0200

    test fixes

--- packages/instrument-newrelic-bull/test/instrument-newrelic.test.js
+++ packages/instrument-newrelic-bull/test/instrument-newrelic.test.js
@@ -1,4 +1,4 @@
-import { BullRedisQueueSystem } from '@stellarjs/queue-redis-bull';
+import { BullRedisQueueSystem } from '@stellarjs/transport-bull';
 import get from 'lodash/get';
 import instrumentBullRedisQueueSystem from '../src';
 
