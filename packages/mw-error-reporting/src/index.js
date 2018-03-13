522b64257ff0cdd9f7d9bba110dd7b2e0858a4ba
udiudi <udi@sheerci.com>
2018-03-13 18:24:26 +0200

    Fixed issues

--- packages/mw-error-reporting/src/index.js
+++ packages/mw-error-reporting/src/index.js
@@ -1,18 +1,21 @@
 import get from 'lodash/get';
 import includes from 'lodash/includes';
 import map from 'lodash/map';
-import forEach from 'lodash/forEach';
 import Promise from 'bluebird';
 
 function errorReportingMiddleware({ reporters, ignoredErrorTypes }) {
-  return function (req, next) {
+  return function (req, next, log) {
     return next()
         .catch((err) => {
           if (includes(map(ignoredErrorTypes, 'name'), get(err, 'constructor.name'))) {
             return Promise.reject(err);
           }
 
-          forEach(reporters, item => item(err, req));
+          try {
+            Promise.map(reporters, reporter => reporter(err, req));
+          } catch (e) {
+            log.error(e);
+          }
           return Promise.reject(err);
         });
   };
