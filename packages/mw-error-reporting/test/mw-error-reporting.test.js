0e5ea92e3b31b5f5fe39676925f8152f8e6cde89
udiudi <udi@sheerci.com>
2018-03-15 13:25:32 +0200

    Added another test for the mw

--- packages/mw-error-reporting/test/mw-error-reporting.test.js
+++ packages/mw-error-reporting/test/mw-error-reporting.test.js
@@ -4,6 +4,8 @@
 
 import Promise from 'bluebird';
 import StellarError from '@stellarjs/stellar-error';
+import { StellarHandler, StellarRequest } from '@stellarjs/core';
+import { MemoryTransport } from '@stellarjs/transport-memory';
 import middleware from '../src';
 
 describe('Error Reporting middleware', () => {
@@ -11,7 +13,37 @@ describe('Error Reporting middleware', () => {
     jest.clearAllMocks();
   });
 
-  it('Should pass request w/o queue to the next midddleware ', (done) => {
+  it('Should only report the first occurence of an error', (done) => {
+    const transport = new MemoryTransport('origin', console);
+    const reporterMock = jest.fn();
+    const request = new StellarRequest(transport);
+    const oneHandler = new StellarHandler(transport);
+    oneHandler.use('.*', middleware({ reporters: [reporterMock] }));
+    const twoHandler = new StellarHandler(transport, 'otherSource');
+    twoHandler.use('.*', middleware({ reporters: [reporterMock] }));
+
+    oneHandler.get('testserviceOne:resource', ({ body }) => {
+      const testserviceRequest = new StellarRequest(transport);
+      return testserviceRequest
+        .get('testserviceTwo:resource')
+        .catch(e => e)
+        .delay(200)
+        .then(e => Promise.reject(e));
+    });
+
+    twoHandler.get('testserviceTwo:resource', ({ body }) => {
+      throw new Error('its broke');
+    });
+
+    request.get('testserviceOne:resource')
+      .catch(e => console.info('received expected error'))
+      .then(() => {
+        expect(reporterMock).toBeCalled();
+      })
+      .then(() => done());
+  });
+
+  it('Should pass request w/o queue to the next middleware ', (done) => {
     const req = {};
     const val = 'noop';
     const next = () => Promise.resolve(val);
@@ -28,9 +60,9 @@ describe('Error Reporting middleware', () => {
   it('Should not report to rollbar a StellarError ', (done) => {
     const req = {};
     const val = 'Boohoo';
-    // const next = () => Promise.resolve(val);
     const reporterMock = jest.fn();
     const mw = middleware({ reporters: [reporterMock], ignoredErrorTypes: [StellarError] });
+
     mw(req, () => Promise.reject(new StellarError('Boohoo')))
           .catch((err) => {
             expect(err.message).toEqual(val);
