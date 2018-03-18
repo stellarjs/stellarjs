023cb6e7ca3ed2fd27936303759ebc13bb99a248
udiudi <udi@sheerci.com>
2018-03-18 14:48:31 +0200

    Report only local errors

--- packages/mw-error-reporting/src/index.js
+++ packages/mw-error-reporting/src/index.js
@@ -3,11 +3,16 @@ import includes from 'lodash/includes';
 import map from 'lodash/map';
 import Promise from 'bluebird';
 
+function isLocalError(err) {
+  const headers = get(err, '__stellarResponse.headers');
+  return get(headers, 'errorSource') === get(headers, 'source');
+}
+
 function errorReportingMiddleware({ reporters, ignoredErrorTypes }) {
   return function (req, next, options, log) {
     return next()
         .catch((err) => {
-          if (includes(map(ignoredErrorTypes, 'name'), get(err, 'constructor.name'))) {
+          if (includes(map(ignoredErrorTypes, 'name'), get(err, 'constructor.name')) || !isLocalError(err)) {
             return Promise.reject(err);
           }
 
