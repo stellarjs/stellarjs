cc3a1efa49035675cba0b6c28a30062043ca8877
orman <orm@gefen.online>
2018-07-23 20:33:11 +0300

    methods.js

--- packages/client-redux/src/index.js
+++ packages/client-redux/src/index.js
@@ -2,3 +2,4 @@ import middleware from './middleware';
 
 export default middleware;
 export * from './getActionType';
+export { default as METHODS } from './methods';
---
+++ packages/client-redux/src/methods.js
@@ -0,0 +1,8 @@
+export default {
+    GET: 'get',
+    CREATE: 'create',
+    UPDATE: 'update',
+    REMOVE: 'remove',
+    SUBSCRIBE: 'subscribe',
+    UNSUBSCRIBE: 'unsubscribe',
+};
