1ebf3764239cbb5ff1c2b0d067d0883f40bb40ee
Moshe Demri <mdemri@gmail.com>
2018-09-05 16:18:33 +0300

    code review changes

--- packages/transport-bull/src/cleaner.js
+++ packages/transport-bull/src/cleaner.js
@@ -1,28 +1,32 @@
 
 import Queue from 'bull';
+import forEach from 'lodash/foreach';
+import keys from 'lodash/keys';
 
 import { DEFAULT_INTERVAL } from './intervals';
 
+const cron = `*/${DEFAULT_INTERVAL} * * * *`; // Every X minutes
+const repeatableJobOptions = {
+  repeat: {
+    cron,
+  },
+  removeOnFail: false,
+  removeOnComplete: true,
+};
+
 function runSubscriberCleaning(transport, log) {
   const queue = new Queue('stlr:periodicTasks', transport.redis.newConnection());
-  const cron = `*/${DEFAULT_INTERVAL} * * * *`; // Every X minutes
-  const repeatableJobOptions = {
-    repeat: {
-      cron,
-    },
-    removeOnFail: false,
-    removeOnComplete: true,
+  const jobs = {
+    'stlr:subscribers:cleaner': () => transport._cleanResources(),
+    'stlr:queues:remover': () => transport._removeUnusedQueues('stlr:*:req'),
   };
 
-  const CLEAN_SUBSCRIBERS_JOB_NAME = 'stlr:subscribers:cleaner';
-  queue.process(CLEAN_SUBSCRIBERS_JOB_NAME, transport._cleanResources());
-  queue.add(CLEAN_SUBSCRIBERS_JOB_NAME, { action: 'run' }, repeatableJobOptions);
-  log.info(`@${CLEAN_SUBSCRIBERS_JOB_NAME}: job is scheduled to run with CRON expression ${cron}`);
-
-  const CLEAN_USUSED_QUEUES_JOB_NAME = 'stlr:queues:remover';
-  queue.process(CLEAN_USUSED_QUEUES_JOB_NAME, transport._removeUnusedQueues('stlr:*:req'));
-  queue.add(CLEAN_USUSED_QUEUES_JOB_NAME, { action: 'run' }, repeatableJobOptions);
-  log.info(`@${CLEAN_USUSED_QUEUES_JOB_NAME}: job is scheduled to run with CRON expression ${cron}`);
+  forEach(keys(jobs), (jobName) => {
+    const job = jobs[jobName];
+    queue.process(jobName, job);
+    queue.add(jobName, { action: 'run' }, repeatableJobOptions);
+    log.info(`@${jobName}: job is scheduled to run with CRON expression ${cron}`);
+  });
 }
 
 // todo function stopCleaner() {
