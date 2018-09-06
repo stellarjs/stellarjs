bdc4c9af7ce61b3000b493417b56be561869a66c
Moshe Demri <mdemri@users.noreply.github.com>
2018-09-06 12:23:10 +0300

    use bull cron jobs, upgrade nr dependencies (#135)
    * use bull cron jobs, upgrade nr dependencies
    
    * code review changes
    
    * make it work
    
    * better forEach

--- packages/transport-bull/src/cleaner.js
+++ packages/transport-bull/src/cleaner.js
@@ -1,30 +1,30 @@
 
-import RedisExclusiveTask from 'redis-exclusive-task';
+import Queue from 'bull';
+import forEach from 'lodash/forEach';
 
 import { DEFAULT_INTERVAL } from './intervals';
 
-let subscriberCleanerRunning = false;
+const cron = `*/${DEFAULT_INTERVAL} * * * *`; // Every X minutes
+const repeatableJobOptions = {
+  repeat: {
+    cron,
+  },
+  removeOnFail: false,
+  removeOnComplete: true,
+};
 
 function runSubscriberCleaning(transport, log) {
-  if (subscriberCleanerRunning) {
-    return;
-  }
+  const queue = new Queue('stlr:periodicTasks', transport.redis.newConnection());
+  const jobs = {
+    'stlr:subscribers:cleaner': () => transport._cleanResources(),
+    'stlr:queues:remover': () => transport._removeUnusedQueues('stlr:*:req'),
+  };
 
-  subscriberCleanerRunning = true; // eslint-disable-line better-mutation/no-mutation
-  log.info(`runSubscriberCleaning`);
-  RedisExclusiveTask.configure([transport.redis.newConnection()], log);
-
-  RedisExclusiveTask.run(
-    'stlr:subscribers:cleaner',
-    () => transport._cleanResources(),
-    DEFAULT_INTERVAL
-  );
-
-  RedisExclusiveTask.run(
-    'stlr:queues:remover',
-    () => transport._removeUnusedQueues('stlr:*:req'),
-    DEFAULT_INTERVAL
-  );
+  forEach(jobs, (job, jobName) => {
+    queue.process(jobName, job);
+    queue.add(jobName, { action: 'run' }, repeatableJobOptions);
+    log.info(`@${jobName}: job is scheduled to run with CRON expression ${cron}`);
+  });
 }
 
 // todo function stopCleaner() {
